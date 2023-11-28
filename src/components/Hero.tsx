68faa19dcbe616d8ca2cde4c4ef26dcb4fb327dc
richzago97 <rich.czago@gmail.com>
2023-11-28 14:24:15 -0300

    feat: created component hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,8 +38,30 @@ const Hero = () => {
                   <Download size={18}/> 
                 </Button>
             </div>
+            <Socials containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+            iconsStyles="text-foreground text-[22px] 
+            hover:text-primary transition-all"
+            />
           </div>
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            <Badge containerStyles="absolute top-[24%] -left-[5rem]"
+             icon={<RiBriefcase4Fill />} endCountNum={2} 
+             badgeText="Years Of Experience"/>
+
+            <Badge containerStyles="absolute top-[80%] -left-[1rem]"
+             icon={<RiTodoFill />} endCountNum={6}
+             badgeText="Finished Projects"/>
+
+            {/* <Badge containerStyles="absolute top-[55%] -right-8"
+             icon={<RiTeamFill />} endCountNum={6}
+             badgeText="Happy Clients"/> */}
+
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark 
+            w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg containerStyles="bg-hero_shape w-[510px] 
+            h-[462px] bg-no-repeat relative bg-bottom" 
+            imgSrc="/hero/developer2.png"/>
+          </div>
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44
         xl:bottom-12 animate-bounce">
