c68af34236c5a3eedf9cddbbfa0796831bc116c8
richzago97 <rich.czago@gmail.com>
2023-12-08 11:16:41 -0300

    feat: added i18n

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,16 +1,16 @@
+import { useTranslations } from "next-intl";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
 const Cta = () => {
+  const t = useTranslations("Cta");
   return (
     <section className="py-24 bg-tertiary dark:bg-secondary/40">
       <div className="container mx-auto">
         <div className="flex flex-col items-center">
-          <h2 className="h2 max-w-xl text-center mb-8">
-            Prepared to turn your ideas into reality? I&apos;m here to help
-          </h2>
+          <h2 className="h2 max-w-xl text-center mb-8">{t("title")}</h2>
           <Link href="/contact">
-            <Button>Contact me</Button>
+            <Button>{t("btn")}</Button>
           </Link>
         </div>
       </div>
